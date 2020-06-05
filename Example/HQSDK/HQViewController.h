c5ce6416d756bb382eb0dcf8a7b740c6b0875c1e
zhanghongwei <327635328@qq.com>
2020-06-05 16:46:45 +0800

    Initial commit

---
+++ Example/HQSDK/HQViewController.h
@@ -0,0 +1,13 @@
+//
+//  HQViewController.h
+//  HQSDK
+//
+//  Created by 327635328 on 06/05/2020.
+//  Copyright (c) 2020 327635328. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HQViewController : UIViewController
+
+@end
---
+++ Example/HQSDK/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HQSDK
+//
+//  Created by 327635328 on 06/05/2020.
+//  Copyright (c) 2020 327635328. All rights reserved.
+//
+
+@import UIKit;
+#import "HQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HQAppDelegate class]));
+    }
+}
