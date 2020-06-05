8b6caba225b624e74fecbf126990ccb83379a670
zhanghongwei <327635328@qq.com>
2020-06-05 17:16:48 +0800

    0.2.0

---
+++ Example/HQSDK/HQCommondController.m
@@ -0,0 +1,32 @@
+//
+//  HQCommondController.m
+//  HQSDK_Example
+//
+//  Created by 张兮兮 on 2020/6/5.
+//  Copyright © 2020 327635328. All rights reserved.
+//
+
+#import "HQCommondController.h"
+
+@interface HQCommondController ()
+
+@end
+
+@implementation HQCommondController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
